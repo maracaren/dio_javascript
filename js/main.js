cd3042700fa0a2d44ee9d3d9cc34c02461afea3b
Maite Renzano <maite.rafaelli@gmail.com>
2020-09-22 18:01:44 -0300

    Criando pag html e script JS

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+console.log(lista);
+lista.pop(1);
+console.log(lista);
+
+
+
+/*
+var nome = "Maite Renzano";
+var idade = 30;
+var num = 10;
+//Se colocar os numeros em aspas duplas ele CONCATENA e não SOMA
+//alert(nome + " tem " + idade + " anos.")
+//alert("Meu primeiro JS");
+alert(idade+num);
+
+console.log(nome);
+// apresenta no console o navegador a mensagem. Só ir em inspecionar e console      
+
+
+var frase = "Japão é o melhor time do mundo.";
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+*/
